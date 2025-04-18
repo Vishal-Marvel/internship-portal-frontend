132cd057456d5348c617e720e69bfff4fc2ddc9b
Vishal-Marvel <vishal.pvn.edu@gmail.com>
2025-04-18 22:41:39 +0530

    Update axios instance to use production baseURL and comment out local development URL

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // url: "https://internship-portal-backend.vercel.app/internship/api/v1",
-  // baseURL: "https://internship-portal-backend.vercel.app/internship/api/v1",
-  baseURL: "http://localhost:3000/internship/api/v1",
+  baseURL: "https://internship-portal-backend.vercel.app/internship/api/v1",
+  // baseURL: "http://localhost:3000/internship/api/v1",
 });
 
 export default axiosInstance;
